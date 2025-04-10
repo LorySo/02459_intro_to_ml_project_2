ae59036b67154463bc2f07b20f787e0cda5d1e1c
LorySo <150460072+LorySo@users.noreply.github.com>
2025-04-10 14:18:07 +0200

    news

--- regression/partbstats.py
+++ regression/partbstats.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import sklearn.model_selection
 import torch
+import scipy.stats
 from baseline import baseline_inner, baseline_outer
 from ann import ann_inner, ann_outer
 from linear_regression import linear_regression_inner, linear_regression_outer
@@ -81,56 +82,48 @@ fixed_hidden_units = 1
 n_rep_ann = 3
 max_iter = 1000
 
-K = 10 
-
 baseline_preds = np.zeros(N)
 rlr_preds = np.zeros(N)
 ann_preds = np.zeros(N)
 
-for k_outer, (train_index_outer, test_index_outer) in enumerate(CV_1.split(X)):
+for (train_index_outer, test_index_outer) in CV_1.split(X):
     X_train_outer = X[train_index_outer]
     y_train_outer = Y[train_index_outer]
     X_test_outer = X[test_index_outer]
     y_test_outer = Y[test_index_outer]
-
-    CV_2 = sklearn.model_selection.KFold(K2, shuffle=True)
-    
-    error_inner_baseline = []
-    error_inner_ann_matrix = []
-    hidden_units_matrix = []
         
     # Baseline predictions
     mean_y = np.mean(y_train_outer)
-    baseline_preds[test_index_outer] = mean_y
+    y_est_test_outer_baseline = mean_y     
+    test_error = np.sum((y_est_test_outer_baseline - y_test_outer)**2) / float(len(y_test_outer))
+    
+    baseline_preds[test_index_outer]=test_error
 
     # ANN
     X_train_outer_tensor = torch.tensor(X_train_outer, dtype=torch.float)
     y_train_outer_tensor = torch.tensor(y_train_outer, dtype=torch.float)
     X_test_outer_tensor = torch.tensor(X_test_outer, dtype=torch.float)
     y_test_outer_tensor = torch.tensor(y_test_outer, dtype=torch.float)
-
-    model = lambda: torch.nn.Sequential(
-            torch.nn.Linear(M, fixed_hidden_units),
-            torch.nn.Tanh(),
-            torch.nn.Linear(fixed_hidden_units, 1)
-        )
     
+    model = lambda: torch.nn.Sequential(
+        torch.nn.Linear(M, fixed_hidden_units),
+        torch.nn.Tanh(),
+        torch.nn.Linear(fixed_hidden_units, 1)
+    )
+
     net, final_loss, learning_curve = train_neural_net(
         model, torch.nn.MSELoss(), X_train_outer_tensor, y_train_outer_tensor,
         n_replicates=n_rep_ann, max_iter=max_iter
     )
 
-    y_ann = net(X_test_outer_tensor)
-    e = (y_ann.float() - y_test_outer_tensor.float())**2
+    y_est_outer = net(X_test_outer_tensor)
+    e = (y_est_outer.float() - y_test_outer_tensor.float())**2
     error_rate = (sum(e).type(torch.float)/len(y_test_outer_tensor)).data.numpy()[0]
     
-    ann_preds[test_index_outer]=error_rate
-
+    ann_preds[test_index_outer] = error_rate
+    
     # RLR
-    model = lm.LinearRegression()
-    model = model.fit(X_train_outer, y_train_outer)
-
-    rlr_preds[test_index_outer]  = model.predict(X_train_outer)
+    rlr_preds[test_index_outer] = linear_regression_outer(train_index_outer, test_index_outer, fixed_lambda)
 
 # Jeffrey Interval and McNemar's Test
 [thetahat_baseline, CIA_baseline] = jeffrey_interval(Y, baseline_preds, alpha=threshold)
@@ -141,16 +134,8 @@ print(f"Baseline, Thetahat: {thetahat_baseline}, CI: {CIA_baseline}")
 print(f"ANN, Thetahat: {thetahat_ann}, CI: {CIA_ann}")
 print(f"RLR, Thetahat: {thetahat_rlr}, CI: {CIA_rlr}")
 
-# Pair evaluations for McNemar's test
-observation_pairs = [
-    (baseline_preds, ann_preds),
-    (baseline_preds, rlr_preds),
-    (ann_preds, rlr_preds)
-]
-pairlist = ["baseline-ann", "baseline-rlr", "ann-rlr"]
 
-for index, pair in enumerate(observation_pairs):
-    obs1, obs2 = pair
-    print(f"\nEvaluating pair: {pairlist[index]}")
-    thetahat, CI, p_value = mcnemar(Y, obs1, obs2, alpha=threshold)
-    print(f"McNemar result for {pairlist[index]}: Thetahat = {thetahat}, CI = {CI}, p-value = {p_value}")
+t_stat, p_value = scipy.stats.ttest_rel(baseline_preds, ann_preds)
+t_stat, p_value = scipy.stats.ttest_rel(baseline_preds, rlr_preds)
+t_stat, p_value = scipy.stats.ttest_rel(ann_preds, rlr_preds)
+
